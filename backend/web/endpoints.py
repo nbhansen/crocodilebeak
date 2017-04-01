9c1e4c667f9f7fc8568dea2661591652fef9c2d7
Nikolai Øllegaard <nikolai@oellegaard.dk>
2017-04-01 16:04:01 +0200

    added POST-ing an rfid-barcode relationship (#2)
    added rfid-barcode relationship

--- backend/web/endpoints.py
+++ backend/web/endpoints.py
@@ -11,6 +11,13 @@ import settings
 logging.basicConfig(level=logging.INFO)
 db = TinyDB(settings.DATA_PATH+"db.json")
 
+logger = logging.getLogger()
 
 def get_rfid(**kwargs):
     return json.dumps(True), 200
+
+def post_product(**kwargs):
+    product = kwargs['product']
+    db.insert(product)
+    logger.info("Inserted rfid %s with barcode %s into db" % (product['rfid'], product['barcode']))
+    return json.dumps(product), 200
