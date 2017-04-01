f51e828e3011c1abe1b38890e842057067ac1bd3
SMonrad <sunemonrad.l@gmail.com>
2017-04-01 11:21:25 +0200

    P5

---
+++ p5-frontend/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+	createCanvas(640, 480);
+}
+
+function draw() {
+	background(255);
+	fill(255);
+
+	rect(50,50,50,50);
+	rect(50,150,50,50);
+	rect(50,250,50,50);
+	rect(50,350,50,50);
+	rect(150,50,50,50);
+  }
